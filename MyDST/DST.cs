d3c76432caba14f8a0df7608c9fd7954223dbe9a
Лужняк Александр <Lyzhniak2005@yandex.ru>
2024-08-07 13:30:03 +0300

    First works

---
+++ MyDST/DST.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDST
+{
+    public class Peak // класс вершины графа
+    {
+        public string Name { get; set; } // 
+        protected int count; // для Дейкстры
+        public List<Peak> IncidnetialPeaks;
+        public Peak()
+        {
+            IncidnetialPeaks = new List<Peak>();
+        }
+        public Peak(string name)
+        {
+            Name = name;
+            IncidnetialPeaks = new List<Peak>();
+        }
+        public override string ToString()
+        {
+            return $"| {Name} Peaks:{string.Join(" ", IncidnetialPeaks.Select(x=>x.Name))} |";
+        }
+    }
+
+    public class Arc // класс дуги графа
+    {
+        public double Weight = 0;
+        public Peak StartPeak;
+        public Peak EndPeak;
+        public bool IsLocated;
+        public Arc(Peak startPeak, Peak endPeak, bool islocated) // для не взешенного графа
+        {
+            StartPeak = startPeak;
+            EndPeak = endPeak;
+            IsLocated = islocated;
+        }
+        public Arc(Peak startPeak, Peak endPeak, double weight, bool islocated) // для взешенного графа
+        {
+            StartPeak = startPeak;
+            EndPeak = endPeak;
+            Weight = weight;
+            IsLocated = islocated;
+        }
+
+        public override string ToString()
+        {
+            return $"Weigth: {Weight} Located: {IsLocated} Peaks:{StartPeak}  {EndPeak}";
+
+        }
+
+
+    }
+    /// <summary>
+    /// Класс графа
+    /// </summary>
+    public class Graph
+    {
+        public IEnumerable<Peak>? Peaks { get; set; }
+        public IEnumerable<Arc>? Arcs { get; set; }
+
+        public int[,] AdjacencyMatrix;
+        public int[,] IncidenceMatrix;
+
+        /// <summary>
+        /// Конструктор создает граф заданный матрицей смежности/инцедентости.
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="typeofmatrix"></param>
+        public Graph(int[,] matrix, int typeofmatrix)
+        {
+            var peaks = new List<Peak>();
+            var arcs = new List<Arc>();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                peaks.Add(new Peak($"{i}"));
+            }
+
+            switch (typeofmatrix)
+            {
+                case 0:
+                    if(matrix.GetLength(0) != matrix.GetLength(1))
+                    {
+                        throw new ArgumentException("It is not adjency matrix!");
+                    }
+                    
+
+                    
+                    
+                   
+
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        var selectedPeak = peaks[i];
+                        for (int j =0;j< matrix.GetLength(0); j++)
+                        {
+                            if (matrix[i,j] != 0)
+                            {
+                                selectedPeak.IncidnetialPeaks.Add(peaks[j]);
+                                arcs.Add(new Arc(selectedPeak, peaks[j],true));
+                            }
+
+
+                        }
+                    }
+                    Peaks=peaks;
+                    Arcs = arcs;
+
+
+                    break;
+
+                case 1:
+                   
+                    for (int j = 0; j < matrix.GetLength(1);j++)
+                    {
+                        Peak start = null;
+                        Peak end = null;
+                        for (int i = 0; i < matrix.GetLength(0); i++)
+                        {
+                            
+
+                            if (matrix[i,j] == 1)
+                            {
+                                end = peaks[i];
+                            }
+                            if (matrix[i, j] == -1)
+                            {
+                                start = peaks[i];
+                            }
+                            if (start != null && end != null)
+                            {
+                                start.IncidnetialPeaks.Add(end);
+                                arcs.Add(new Arc(start, end, true));
+
+                            }
+
+                        }
+                        
+
+
+                    }
+
+                    Peaks = peaks;
+                    Arcs = arcs;
+
+
+                    break;
+
+
+                default:
+                    throw new ArgumentException("Invalid type of matrix!");
+
+            }
+
+
+
+            
+        }
+       
+
+
+    }
+
+
+
+
+}
--- MyDST/Program.cs
+++ MyDST/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyDST;
+public class Programm
+{
+    public static int[,] Data = { { 0, 1,1 }, { 1, 0,1 },{ 1, 1,0 } };
+    public static int[,] Dta = { 
+        { 0, 1, -1 },
+        { 1, 0, 1 }, 
+        { -1, 1, 0 } 
+    };
+    static void Main(string[] args)
+    {
+        var g = new Graph(Data, 0);
+
+
+       foreach(var x in g.Arcs)
+        {
+            Console.WriteLine(x.ToString());
+        }
+        g = new Graph(Dta, 1);
+        Console.WriteLine("Warning!");
+        foreach (var x in g.Arcs)
+        {
+            Console.WriteLine(x.ToString());
+        }
+    }
+}
