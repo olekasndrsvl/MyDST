251db5942eb1d1bce64e725999c5b8b5ae8af162
Лужняк Александр <Lyzhniak2005@yandex.ru>
2024-08-08 16:58:54 +0300

    Добавлен алгоритм Краскала и Дейкстры

--- MyDST/DST.cs
+++ MyDST/DST.cs
@@ -9,7 +9,10 @@ namespace MyDST
     public class Peak // класс вершины графа
     {
         public string Name { get; set; } // 
-        protected int count; // для Дейкстры
+        /// <summary>
+        /// При применении алгоритма Дейкстры в данное поле будет записана длина кратчайшего пути из стартовой вершины в данную.
+        /// </summary>
+        public double count = -1; // для Дейкстры
         public List<Peak> IncidnetialPeaks;
         public Peak()
         {
@@ -25,30 +28,36 @@ namespace MyDST
             return $"| {Name} Peaks:{string.Join(" ", IncidnetialPeaks.Select(x=>x.Name))} |";
         }
     }
-
-    public class Arc // класс дуги графа
+    /// <summary>
+    /// Класс дуги графа
+    /// </summary>
+    public class Arc
     {
         public double Weight = 0;
         public Peak StartPeak;
         public Peak EndPeak;
         public bool IsLocated;
-        public Arc(Peak startPeak, Peak endPeak, bool islocated) // для не взешенного графа
+        public Arc(Peak startPeak, Peak endPeak, bool islocated=true) // для не взешенного графа
         {
             StartPeak = startPeak;
             EndPeak = endPeak;
             IsLocated = islocated;
         }
-        public Arc(Peak startPeak, Peak endPeak, double weight, bool islocated) // для взешенного графа
+        public Arc(Peak startPeak, Peak endPeak, double weight, bool islocated=true) // для взешенного графа
         {
             StartPeak = startPeak;
             EndPeak = endPeak;
             Weight = weight;
             IsLocated = islocated;
         }
-
+        public int CompareTo(Arc other)
+        {
+            if (other == null) return 1;
+            return Weight.CompareTo(other.Weight);
+        }
         public override string ToString()
         {
-            return $"Weigth: {Weight} Located: {IsLocated} Peaks:{StartPeak}  {EndPeak}";
+            return $"Weigth: {Weight} Located: {IsLocated} Peaks:{StartPeak.Name}  {EndPeak.Name}";
 
         }
 
@@ -59,11 +68,117 @@ namespace MyDST
     /// </summary>
     public class Graph
     {
+        /// <summary>
+        /// Все вершины графа
+        /// </summary>
         public IEnumerable<Peak>? Peaks { get; set; }
+        /// <summary>
+        /// Все дуги графа
+        /// </summary>
         public IEnumerable<Arc>? Arcs { get; set; }
 
+        /// <summary>
+        /// Матрица смежности графа
+        /// </summary>
         public int[,] AdjacencyMatrix;
+        /// <summary>
+        /// Матрица инцидентности графа
+        /// </summary>
         public int[,] IncidenceMatrix;
+
+
+
+        /// <summary>
+        /// Алгоритм поиска Краскаловского дерева(Остовного дерева графа). Возвращает IEnumerable коллекцию дуг остовного дерева.
+        /// </summary>
+        /// <param name="startPeak"></param>
+        /// <returns></returns>
+        public static IEnumerable<Arc> DST(Peak startPeak)
+        {
+            List<Peak> visitedPeaks = new List<Peak> ();
+            visitedPeaks.Add (startPeak);
+            List<Arc> result = new List<Arc> ();
+
+            // Рекурсивный обход графа.
+            void dst(Peak peak)
+            {
+                var tmp = peak.IncidnetialPeaks;
+                foreach(var x in tmp)
+                {
+                    Console.WriteLine(x);
+                    if(!visitedPeaks.Contains(x))
+                    {
+                        visitedPeaks.Add(x);
+                        result.Add(new Arc(peak, x, true));
+                        dst(x);
+
+                      
+                    }
+                }
+
+
+
+            }
+
+            dst(startPeak);
+            return result;
+
+
+
+
+
+
+        }
+
+
+
+
+        public void Dijkstra(Peak start)
+        {
+            start.count = 0;
+
+            void d_algorithm(Peak peak)
+            {
+                foreach(var x in Arcs)
+                {
+                    if(x.StartPeak == peak)
+                    {
+                        if (x.EndPeak.count != -1)
+                        {
+                            x.EndPeak.count= Math.Min(x.EndPeak.count, x.StartPeak.count+x.Weight);
+                        }
+                        else
+                        {
+                            x.EndPeak.count = x.StartPeak.count+ x.Weight;
+                        }
+                    }
+                }
+
+                foreach (var x in peak.IncidnetialPeaks)
+                {
+                    d_algorithm(x);
+                }
+
+
+
+            }
+
+            d_algorithm(start);
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
 
         /// <summary>
         /// Конструктор создает граф заданный матрицей смежности/инцедентости.
@@ -100,7 +215,7 @@ namespace MyDST
                             if (matrix[i,j] != 0)
                             {
                                 selectedPeak.IncidnetialPeaks.Add(peaks[j]);
-                                arcs.Add(new Arc(selectedPeak, peaks[j],true));
+                                arcs.Add(new Arc(selectedPeak, peaks[j], matrix[i,j],true));
                             }
 
 
@@ -118,29 +233,34 @@ namespace MyDST
                     {
                         Peak start = null;
                         Peak end = null;
+                        int weight = 0;
                         for (int i = 0; i < matrix.GetLength(0); i++)
                         {
-                            
+                            weight = Math.Max(weight, matrix[i, j]);
 
-                            if (matrix[i,j] == 1)
+                            if ((matrix[i, j] ) >0)
                             {
                                 end = peaks[i];
                             }
-                            if (matrix[i, j] == -1)
+                            if ((matrix[i, j] ) <0 )
                             {
                                 start = peaks[i];
                             }
                             if (start != null && end != null)
                             {
                                 start.IncidnetialPeaks.Add(end);
-                                arcs.Add(new Arc(start, end, true));
+                                arcs.Add(new Arc(start, end, weight,true));
+                                start = null;
+                                end = null;
 
                             }
+                            Console.Write(matrix[i, j]+" #"+i+"  ");
+                            
 
                         }
-                        
 
 
+                        Console.WriteLine();
                     }
 
                     Peaks = peaks;
--- MyDST/Program.cs
+++ MyDST/Program.cs
@@ -3,9 +3,10 @@ public class Programm
 {
     public static int[,] Data = { { 0, 1,1 }, { 1, 0,1 },{ 1, 1,0 } };
     public static int[,] Dta = { 
-        { 0, 1, -1 },
-        { 1, 0, 1 }, 
-        { -1, 1, 0 } 
+        { -2, 0, 0, -5,-19 },
+        { 2, -1, 0, 0,0 }, 
+        { 0, 1, -1,5, 0},
+        { 0, 0, 1, 0,19}
     };
     static void Main(string[] args)
     {
@@ -17,10 +18,24 @@ public class Programm
             Console.WriteLine(x.ToString());
         }
         g = new Graph(Dta, 1);
+        g.Dijkstra(g.Peaks.First());
         Console.WriteLine("Warning!");
         foreach (var x in g.Arcs)
         {
             Console.WriteLine(x.ToString());
         }
+        foreach (var x in g.Peaks)
+        {
+            Console.WriteLine(x.count);
+        }
+
+        Console.WriteLine("test dst");
+        var t = Graph.DST(g.Peaks.First()).ToList();
+        
+        foreach (var x in t)
+        {
+            Console.WriteLine(x);
+        }
+        
     }
 }
